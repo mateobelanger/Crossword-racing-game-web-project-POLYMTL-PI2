4dd761fa37fa596f5c7b2c16bb28a06454ce48bd
HuppeJ <30572358+HuppeJ@users.noreply.github.com>
2018-01-22 12:36:31 -0500

    Warning (RouterModule not used) fixed - RG - J.H.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import { RouterModule } from '@angular/router';
+//import { RouterModule } from '@angular/router';
 
 import { AppComponent } from "./app.component";
 import { GameComponent } from "./racing-game/game-component/game.component";
