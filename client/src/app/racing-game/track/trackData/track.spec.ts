c5cd4a4cd969c0eb5a92bee5c2f8e7a9c160ae5c
poclecoqq <pierre-olivier.cote@polymtl.ca>
2018-01-30 18:23:10 -0500

    modified track because of the modifications to waypoint - RG - POC

--- client/src/app/racing-game/track/trackData/track.spec.ts
+++ client/src/app/racing-game/track/trackData/track.spec.ts
@@ -3,17 +3,20 @@ import { Vector3 } from "three";
 import { Waypoint} from './waypoint';
  
 describe("Track", () => {
-    let Waypoints : Waypoint[] = [];
+    let waypoints : Waypoint[] = [];
     let track: Track;
  
     beforeEach( () => {
-        for (let i: number = 0; i < 10 ; i++) 
-        Waypoints.push(new Waypoint(new Vector3(i * 10, 0, 0), i));
-        track = new Track(Waypoints);
+        for (let i: number = 0; i < 10 ; i++) {
+            let waypoint : Waypoint = new Waypoint(new Vector3(i * 10, 0, 0));
+            waypoint.bindCircle(i);//mock circleId
+            waypoints.push(waypoint);
+        }
+        track = new Track(waypoints);
     });
  
     afterEach(() => {
-        Waypoints = []
+        waypoints = []
         track = undefined;
     });
  
@@ -25,22 +28,30 @@ describe("Track", () => {
 
     it("constructor with parameters", ()=>{   
         expect(track).toBeDefined();
-        expect(track.getWaypoints()).toBe(Waypoints);      
+        expect(track.getWaypoints()).toBe(waypoints);      
     });
  
+    it("getWaypoints should return array of waypoint", ()=>{
+        expect(track.getWaypoints()).toBeDefined();
+        expect(track.getWaypoints().length).toBe(10);
+    });
+
+    it("getWaypoint should return matching circle id waypoint", ()=>{
+        expect(track.getWaypoint(2)).toBeDefined();
+        expect(track.getWaypoint(2).getPosition()).toEqual(new Vector3(20, 0, 0));
+    });
+
     it("addWaypoint", () => {
-        let waypoint: Waypoint = new Waypoint( new Vector3 (1,1,1), 100)
+        let waypoint: Waypoint = new Waypoint( new Vector3 (1,1,1));
+        waypoint.bindCircle(10);//mock circle id
         track.addWaypoint(waypoint);
-        Waypoints.push(waypoint);
-        expect(track.getWaypoint(100)).toBe(waypoint);
+        waypoints.push(waypoint);
+        expect(track.getWaypoint(10)).toBe(waypoint);
     });
 
     it("removeWaypoint", () => {
         expect(track.getWaypoints().length).toBe(10);
-        track.removeWaypoint(1);
-        expect(track.getWaypoints().length).toBe(9);
-        //checking if the right one has been removed
-        track.removeWaypoint(1);
+        track.removeWaypoint();
         expect(track.getWaypoints().length).toBe(9);
     });
  
@@ -48,4 +59,6 @@ describe("Track", () => {
         track.modifyWaypointPosition(2, new Vector3(0,5,0));
         expect(track.getWaypoint(2).getPosition().y).toBe(5);
     });
+
+    
 });
