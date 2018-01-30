0e8c3b90e7598b508cb43442395ed5b2519b74e9
Samuel Meilleur <samuel.meilleur@outlook.com>
2018-01-30 15:14:09 -0500

    Created Word struct - CG - S.M.

---
+++ server/app/crossword-game/word.ts
@@ -0,0 +1,10 @@
+
+export class Word {
+    public row: number;
+    public column: number;
+    public direction: string;
+    public length: number;
+
+    public value: string;
+    public definition: string;
+}
