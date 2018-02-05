19831a0232fe46d20176e665ca0c24b392979bd3
Jeremie Huppe <jeremie.huppe@polymtl.ca>
2018-02-05 12:19:50 -0500

    Update of plane tests - RG - J.H.

--- client/src/app/racing-game/track/trackBuildingBlocks/plane.ts
+++ client/src/app/racing-game/track/trackBuildingBlocks/plane.ts
@@ -12,7 +12,7 @@ const PLANE_POSITION_Z: number = -1;
 
 export class Plane {
 
-    private beginPoint: THREE.Vector3;
+    private beginingPoint: THREE.Vector3;
 
     private endPoint: THREE.Vector3;
 
@@ -25,7 +25,7 @@ export class Plane {
         this.previousAngle = 0;
         waypoint1.setPositionZ(PLANE_POSITION_Z);
         waypoint2.setPositionZ(PLANE_POSITION_Z);
-        this.beginPoint = waypoint1.getPosition();
+        this.beginingPoint = waypoint1.getPosition();
         this.endPoint = waypoint2.getPosition();
         this.mesh = null;
     }
@@ -40,7 +40,7 @@ export class Plane {
 
     public calculateRadianAngle(): number {
         const directionVector: THREE.Vector3 = new THREE.Vector3();
-        directionVector.subVectors(this.endPoint, this.beginPoint);
+        directionVector.subVectors(this.endPoint, this.beginingPoint);
         let angle: number = directionVector.angleTo(REFERENCE_VECTOR);
         if (directionVector.y < REFERENCE_VECTOR.y)
             angle *= -1;
@@ -61,19 +61,19 @@ export class Plane {
     }
 
     public getLength(): number {
-        return Math.sqrt(Math.pow(this.beginPoint.x - this.endPoint.x, EXPOSANT_CARRE)
-                                + Math.pow(this.beginPoint.y - this.endPoint.y, EXPOSANT_CARRE)
-                                + Math.pow(this.beginPoint.z - this.endPoint.z, EXPOSANT_CARRE))
+        return Math.sqrt(Math.pow(this.beginingPoint.x - this.endPoint.x, EXPOSANT_CARRE)
+                                + Math.pow(this.beginingPoint.y - this.endPoint.y, EXPOSANT_CARRE)
+                                + Math.pow(this.beginingPoint.z - this.endPoint.z, EXPOSANT_CARRE))
                                 - CIRLEDIAMETER;
     }
 
     public getCenterPoint(): THREE.Vector3 {
-        const centerPoint: THREE.Vector3 = new THREE.Vector3((this.endPoint.x - this.beginPoint.x) / DIVISEUR_MOYENNE,
-                                                             (this.endPoint.y - this.beginPoint.y) / DIVISEUR_MOYENNE,
-                                                             (this.endPoint.z - this.beginPoint.z) / DIVISEUR_MOYENNE
+        const centerPoint: THREE.Vector3 = new THREE.Vector3((this.endPoint.x - this.beginingPoint.x) / DIVISEUR_MOYENNE,
+                                                             (this.endPoint.y - this.beginingPoint.y) / DIVISEUR_MOYENNE,
+                                                             (this.endPoint.z - this.beginingPoint.z) / DIVISEUR_MOYENNE
                                                     );
 
-        return centerPoint.add(this.beginPoint);
+        return centerPoint.add(this.beginingPoint);
     }
 
     public setEndPoint(newPos: THREE.Vector3): THREE.Vector3 {
@@ -88,13 +88,13 @@ export class Plane {
     }
 
     public setBeginingPoint(newPos: THREE.Vector3): THREE.Vector3 {
-        const oldBeginPoint: THREE.Vector3 = this.beginPoint;
-        this.beginPoint = newPos;
+        const oldBeginPoint: THREE.Vector3 = this.beginingPoint;
+        this.beginingPoint = newPos;
 
         return oldBeginPoint;
     }
 
     public getBeginingPoint(): THREE.Vector3 {
-        return this.beginPoint;
+        return this.beginingPoint;
     }
 }
