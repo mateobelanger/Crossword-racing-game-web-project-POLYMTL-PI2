9830320a8aaed8878eb037f4951493f6f47c0095
Jeremie Huppe <jeremie.huppe@polymtl.ca>
2018-02-05 23:01:09 -0500

    Fixed bugs after the merge - RG & CG - J.H

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
 
@@ -21,7 +20,7 @@ import { routes } from './app-routes.module';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
     ],
     imports: [
         BrowserModule,
@@ -30,6 +29,7 @@ import { routes } from './app-routes.module';
         HomepageModule,
         CrosswordGameModule,
         RacingGameModule,
+        AdminModule
 
     ],
     providers: [ ],
