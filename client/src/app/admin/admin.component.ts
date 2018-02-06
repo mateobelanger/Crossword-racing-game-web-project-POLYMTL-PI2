ddc291b2d8230698455b2fa08e62d7218f52ea1e
Jeremie Huppe <jeremie.huppe@polymtl.ca>
2018-02-05 21:11:28 -0500

    Fixed Tsling error in admin.component - RG -J.H

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  public constructor() { }
 
-  ngOnInit() {
+  public ngOnInit(): void {
   }
 
 }
