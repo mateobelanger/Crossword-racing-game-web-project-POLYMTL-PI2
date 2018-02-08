7a447fa1a9bf3bde0873206d1f088556d1458ef2
Jeremie Huppe <jeremie.huppe@polymtl.ca>
2018-02-08 14:56:09 -0500

    Fixed Karma error : link between app-track-editor and admin component - RG - J.H.

--- client/src/app/admin/admin.component.spec.ts
+++ client/src/app/admin/admin.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminComponent } from './admin.component';
+import { AdminModule } from './admin.module';
 
 describe('AdminComponent', () => {
     let component: AdminComponent;
@@ -8,7 +9,7 @@ describe('AdminComponent', () => {
 
     beforeEach(async(() => {
       TestBed.configureTestingModule({
-        declarations: [ AdminComponent ]
+        imports : [ AdminModule ]
       })
       .compileComponents();
     }));
