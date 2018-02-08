13dc77a551806f0d058ee1e093e37e918556931a
Jeremie Huppe <jeremie.huppe@polymtl.ca>
2018-02-08 14:48:20 -0500

    Fixed AdminModule not used in admin.component.spec.ts - RG - J.H.

--- client/src/app/admin/admin.component.spec.ts
+++ client/src/app/admin/admin.component.spec.ts
@@ -1,18 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminComponent } from './admin.component';
-import { AdminModule } from './admin.module';
 
 describe('AdminComponent', () => {
     let component: AdminComponent;
     let fixture: ComponentFixture<AdminComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ AdminComponent ]
+      })
+      .compileComponents();
+    }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(AdminComponent);
