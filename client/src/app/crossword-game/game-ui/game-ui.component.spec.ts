66cd376e604cb13c166d3e73d6061cf9f477798a
Jeremie Huppe <jeremie.huppe@polymtl.ca>
2018-02-09 19:45:57 -0500

    Fixed warnings : deleted non-used imports - RG & CG - J.H.

--- client/src/app/crossword-game/game-ui/game-ui.component.spec.ts
+++ client/src/app/crossword-game/game-ui/game-ui.component.spec.ts
@@ -2,10 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { APP_BASE_HREF } from '@angular/common';
 
 import { GameUiComponent } from './game-ui.component';
-import { InformationsComponent } from '../informations/informations.component';
-import { GridComponent } from '../grid/grid.component';
-import { DefinitionsComponent } from '../definitions/definitions.component';
-import { FormsModule } from '@angular/forms';
 import { routes } from '../../app-routes.module';
 import { AppModule } from '../../app.module';
 
--- client/src/app/racing-game/racing-game.component.spec.ts
+++ client/src/app/racing-game/racing-game.component.spec.ts
@@ -2,7 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { APP_BASE_HREF } from '@angular/common';
 
 import { RacingGameComponent } from './racing-game.component';
-import { RacingGameModule } from './racing-game.module';
 import { routes } from '../app-routes.module';
 import { AppModule } from '../app.module';
 
