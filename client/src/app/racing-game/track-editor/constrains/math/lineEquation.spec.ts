19a872f42c553f465cd28c3327a9bfa37fd2a139
poclecoqq <pierre-olivier.cote@polymtl.ca>
2018-02-11 11:05:42 -0500

    lineEquation functionnal - RG - POC

--- client/src/app/racing-game/track-editor/constrains/math/lineEquation.spec.ts
+++ client/src/app/racing-game/track-editor/constrains/math/lineEquation.spec.ts
@@ -5,34 +5,92 @@ import * as THREE from "three";
 describe("lineEquation", () => {
     const firstPoint: Point = new Point(new THREE.Vector3(-5, 4, 10));
     const secondPoint: Point  = new Point(new THREE.Vector3(11, 6, 10));
-    const line: LineEquation = new LineEquation();
-    line.initialize(firstPoint, secondPoint);
+    const line: LineEquation = new LineEquation(firstPoint, secondPoint);
+    line.initialize();
 
 
     it("should be instanciable with default constructor", () => {
+        const aline: LineEquation = new LineEquation();
+        expect(aline).toBeDefined();
+    });
+
+    it("should be instanciable with constructor with parameters", () => {
         expect(line).toBeDefined();
     });
-    it("get lower bound", () => {
-        expect(line.lowerBound).toBe(-5);
+
+    it("get begin point", () => {
+        const expectedPoint: Point = new Point(new THREE.Vector3(-5, 4));
+        expect(line.beginPoint).toEqual(expectedPoint);
     });
-    it("get higher bound", () => {
-        expect(line.higherBound).toBe(11);
+
+    it("get endPoint", () => {
+        const expectedPoint: Point = new Point(new THREE.Vector3(11, 6));
+        expect(line.endPoint).toEqual(expectedPoint);
     });
+
     it("get slope", () => {
         expect(line.slope).toBe(1 / 8);
     });
+
     it("get b", () => {
         expect(line.b).toBe(4.625);
     });
+
     it("isVerticaLine with regular line", () => {
         expect(line.isVerticalLine).not.toBeTruthy();
     });
+
     it("isVerticaLine with vertical line (not a function)", () => {
         const aFirstPoint: Point = new Point(new THREE.Vector3(0, 4, 10));
         const aSecondPoint: Point  = new Point(new THREE.Vector3(0, 6, 10));
-        const aLine: LineEquation = new LineEquation();
-        aLine.initialize(aFirstPoint, aSecondPoint);
+        const aLine: LineEquation = new LineEquation(aFirstPoint, aSecondPoint);
+        aLine.initialize();
         expect(aLine.isVerticalLine).toBeTruthy();
+    });
+
+    it("lineInDomain should be truthy", () => {
+        const aFirstPoint: Point = new Point(new THREE.Vector3(0, 4, 10));
+        const aSecondPoint: Point  = new Point(new THREE.Vector3(0, 6, 10));
+        const aLine: LineEquation = new LineEquation(aFirstPoint, aSecondPoint);
+        aLine.initialize();
+        expect(line.lineInDomain(aLine)).toBeTruthy();
+    });
+
+    it("lineInDomain should be false", () => {
+        const aFirstPoint: Point = new Point(new THREE.Vector3(-20, 4, 10));
+        const aSecondPoint: Point  = new Point(new THREE.Vector3(-50, 6, 10));
+        const aLine: LineEquation = new LineEquation(aFirstPoint, aSecondPoint);
+        aLine.initialize();
+        expect(line.lineInDomain(aLine)).not.toBeTruthy();
+    });
+
+    it("lineInImage should be truthy", () => {
+        const aFirstPoint: Point = new Point(new THREE.Vector3(0, 4, 10));
+        const aSecondPoint: Point  = new Point(new THREE.Vector3(0, 6, 10));
+        const aLine: LineEquation = new LineEquation(aFirstPoint, aSecondPoint);
+        aLine.initialize();
+        expect(line.lineInImage(aLine)).toBeTruthy();
+    });
+
+    it("lineInImage should be false", () => {
+        const aFirstPoint: Point = new Point(new THREE.Vector3(0, 80, 10));
+        const aSecondPoint: Point  = new Point(new THREE.Vector3(0, 100, 10));
+        const aLine: LineEquation = new LineEquation(aFirstPoint, aSecondPoint);
+        aLine.initialize();
+        expect(line.lineInImage(aLine)).not.toBeTruthy();
+    });
+
+    it("image", () => {
+        expect(line.image(19)).toBe(7);
+    });
+
+    it("intersection", () => {
+        const aFirstPoint: Point = new Point(new THREE.Vector3(1, 4, 10));
+        const aSecondPoint: Point  = new Point(new THREE.Vector3(7, 5, 0));
+        const aLine: LineEquation = new LineEquation(aFirstPoint, aSecondPoint);
+        aLine.initialize();
+        const expectedIntersection: Point = new Point(new THREE.Vector3(19, 7));
+        expect(line.intersection(aLine)).toEqual(expectedIntersection);
     });
 
 });
--- client/src/app/racing-game/track-editor/constrains/math/lineEquation.ts
+++ client/src/app/racing-game/track-editor/constrains/math/lineEquation.ts
@@ -1,5 +1,5 @@
 import { Point } from "./point";
-
+import * as THREE from "three";
 export class LineEquation {
 
     // y = ax + b
@@ -7,29 +7,35 @@ export class LineEquation {
 
     private _b: number;
 
-    private _lowerBound: number;
-
-    private _higherBound: number;
-
     private _isVerticalLine: boolean = false;
 
-    public constructor() {
+    private _beginPoint: Point;
+
+    private _endPoint: Point;
+
+    public constructor( beginPoint: Point = new Point(new THREE.Vector3()),
+                        endPoint: Point= new Point(new THREE.Vector3())) {
+        if (beginPoint.x < endPoint.y) {
+            this._beginPoint = beginPoint;
+            this._endPoint = endPoint;
+        } else {
+            this._beginPoint = endPoint;
+            this._endPoint = beginPoint;
+        }
     }
 
-    public initialize(firstPoint: Point, secondPoint: Point): void {
-        this._lowerBound = firstPoint.x;
-        this._higherBound = secondPoint.x;
-        if ( firstPoint.x === secondPoint.x )
+    public initialize(): void {
+        if ( this.beginPoint.x === this.endPoint.x )
             this._isVerticalLine = true;
-        this.calculateLineEquation(firstPoint, secondPoint);
+        this.calculateLineEquation(this._beginPoint, this._endPoint);
     }
 
-    public get higherBound(): number {
-        return this._higherBound;
+    public get beginPoint(): Point {
+        return this._beginPoint;
     }
 
-    public get lowerBound(): number {
-        return this._lowerBound;
+    public get endPoint(): Point {
+        return this._endPoint;
     }
 
     public get slope(): number {
@@ -42,6 +48,32 @@ export class LineEquation {
 
     public get isVerticalLine(): boolean {
         return this._isVerticalLine;
+    }
+
+    public lineInDomain(line: LineEquation): boolean {
+        return (line.beginPoint.x <= this.endPoint.x && line.endPoint.x >= this.beginPoint.x);
+    }
+
+    public xInDomain(x: number): boolean {
+        return (this.beginPoint.x <= x || this.endPoint.x >= x);
+    }
+
+    public lineInImage(line: LineEquation): boolean {
+        return (line.beginPoint.y <= this.endPoint.y && line.endPoint.y >= this.beginPoint.y);
+    }
+    public yInImage(y: number): boolean {
+        return (this.beginPoint.y <= y || this.endPoint.y >= y);
+    }
+
+    public intersection(line: LineEquation): Point {
+        const xInstersection: number = (line.b - this.b) / (this.slope - line.slope);
+        const yInstersection: number = this.image(xInstersection);
+
+        return new Point( new THREE.Vector3(xInstersection, yInstersection));
+    }
+
+    public image(x: number): number {
+        return x * this.slope + this.b;
     }
 
     private calculateLineEquation(firstPoint: Point, secondPoint: Point): void {
