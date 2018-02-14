d7fddd1bc3d540f613d7dd097a39cdb1aceec3f6
AlexandreRault <alexandre.rault@polymtl.ca>
2018-02-14 10:18:23 -0500

    Added comments with credentials to connect to the mLab mongoDB - DB - A.R.

--- server/app/mongoDBAccess.ts
+++ server/app/mongoDBAccess.ts
@@ -1,16 +1,26 @@
 import { Response } from "express";
 
-// const requestPromise = require("request-promise-native");
+// mLab account:
+// username: arault@outlook.com
+// password: *x-qGMlC1vwh
+
+// Connection URL
+// const userName: string = "admin";
+// const password: string = "admin";
+// const url: string = "mongodb://" + userName + ":" + password + "@ds115436.mlab.com:15436";
+ 
+// Database Name
+// const dbName = "log2990-equipe4";
 
 export class MongoDBAccess {
-
+    
     public constructor() {
 
     }
 
-    public helloWorld(res: Response): void {         
+    public helloWorld(res: Response): void {
 
         res.send("Hello World");
     }
-
+ 
 }
