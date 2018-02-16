f435dabca9682df078cc05dd92d3925b636229f2
geboca <genevieve.bock@polymtl.ca>
2018-02-16 12:34:15 -0500

    removing track by name functionnal - RG - G.B.

--- server/app/mongoDBService.ts
+++ server/app/mongoDBService.ts
@@ -1,5 +1,5 @@
 import { injectable} from "inversify";
-import { Router, Request, Response, NextFunction } from "express";
+import { Router, Request, Response } from "express";
 import { MongoDBAccess } from "./mongoDBAccess";
 
 @injectable()
@@ -8,24 +8,41 @@ export class MongoDBService {
     public constructor() {}
 
     public get routes(): Router {
-        const mongoDBAccess: MongoDBAccess = new MongoDBAccess();
         const router: Router = Router();
 
         router.get("/service/mongoDB",
-            (req: Request, res: Response, next: NextFunction) => {
-                mongoDBAccess.helloWorld(res);
-            });
+                (req: Request, res: Response) => {
+                    MongoDBAccess.getAll(req, res);
+                });
 
 
-        router.get("/service/mongoDB/test",
-            (req: Request, res: Response, next: NextFunction) => {
-                mongoDBAccess.testConnection(res);
-            });
+        router.post("/service/mongoDB",
+                    (req: Request, res: Response) => {
+                        MongoDBAccess.addTrack(req, res);
+                    });
 
-        router.get("/service/mongoDB/test2",
-            (req: Request, res: Response, next: NextFunction) => {
-                mongoDBAccess.testElement(res);
-            });
+        
+        router.delete(  "/service/mongoDB/:name", 
+                        (req: Request, res: Response) =>
+                            MongoDBAccess.remove(res, req.params.name)
+                        );
+        
+        // custom 404 page
+        router.use((req: Request, res: Response) => {
+            res.type('text/plain');
+            res.status(404);
+            res.send('404 - Not Found');
+        });
+        
+        
+        // function handleServerError(response: Response, error: string){
+        //     console.log(error);
+        //     response.type('text/plain');
+        //     response.status(500);
+        //     response.send('500 - Server Error');
+        // }
+        
+
 
         return router;
 
