cb80c5d3549985b95ac6da66b3acf673cbf73d2e
Samuel Meilleur <samuel.meilleur@outlook.com>
2018-02-17 16:24:47 -0500

    Created mock-words to test the word selection - CG - S.M.

---
+++ client/src/app/crossword-game/mock-words.ts
@@ -0,0 +1,8 @@
+import { Word, Direction } from '../../../../common/word';
+
+
+export const words: Word[] = [
+    {row: 0, column: 0, direction: Direction.Horizontal, size: 3, value: "sit", definition: "I like to ___ on my ass." },
+    {row: 0, column: 5, direction: Direction.Horizontal, size: 5, value: "value", definition: "The word is value." },    
+    {row: 0, column: 0, direction: Direction.Vertical, size: 3, value: "sat", definition: "This definition is actually really long because like I needed to do a test for something that I wasn't sure about." }
+]
