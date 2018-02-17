ba3468a574420175aece96f313214dcb9a3766db
Jeremie Huppe <jeremie.huppe@polymtl.ca>
2018-02-17 16:55:57 -0500

    Added some tests to camera.service - RG - J.H

--- client/src/app/racing-game/camera.service.spec.ts
+++ client/src/app/racing-game/camera.service.spec.ts
@@ -1,15 +1,76 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { CameraService } from './camera.service';
+import { CameraService, ORTHOGRAPHIC_INITIAL_POSITION_Y } from './camera.service';
+import { Car } from './car/car';
+import { Object3D } from 'three';
+import { Engine } from './car/engine';
+
+const MS_BETWEEN_FRAMES: number = 16.6667;
+
+/* tslint:disable: no-magic-numbers */
+class MockEngine extends Engine {
+  public getDriveTorque(): number {
+      return 10000;
+  }
+}
 
 describe('CameraService', () => {
-  beforeEach(() => {
+
+  const dummyElement: HTMLDivElement = document.createElement('div');
+  const cameraService: CameraService = new CameraService();
+  let car: Car;
+  let target: Object3D = new Object3D();
+
+  beforeEach(async (done: () => void) => {
     TestBed.configureTestingModule({
       providers: [CameraService]
     });
+    car = new Car(new MockEngine());
+    await car.init();
+
+    car.isAcceleratorPressed = true;
+    car.update(MS_BETWEEN_FRAMES);
+    car.isAcceleratorPressed = false;
+
+    target = car.mesh;
+    cameraService.initialize(dummyElement, target);
+
+    done();
   });
 
   it('should be created', inject([CameraService], (service: CameraService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should have a target defined', () => {
+    expect(target).toBeTruthy();
+  });
+
+  it('should have the same initial position for perpective camera and target', () => {
+    // TODO : verify
+    expect(cameraService.perspectiveCamera.position.x).toBeCloseTo(target.position.x);
+    expect(cameraService.perspectiveCamera.position.y).toBeCloseTo(target.position.y);
+    expect(cameraService.perspectiveCamera.position.z).toBeCloseTo(target.position.z);
+  });
+
+  it('should have the same initial position for orthographic camera and target', () => {
+    // TODO : verify
+    expect(cameraService.orthographicCamera.position.x).toBeCloseTo(target.position.x);
+    expect(cameraService.orthographicCamera.position.y).toBeCloseTo(ORTHOGRAPHIC_INITIAL_POSITION_Y);
+    expect(cameraService.orthographicCamera.position.z).toBeCloseTo(target.position.z);
+  });
+
+  it('moving', () => {
+    // TODO : add some tests 
+    /*
+    car.isAcceleratorPressed = true;
+    car.update(MS_BETWEEN_FRAMES);
+    car.isAcceleratorPressed = false;
+
+    expect(cameraService.perspectiveCamera.position.x).toBeCloseTo(target.position.x);
+    expect(cameraService.perspectiveCamera.position.z).toBeCloseTo(target.position.z);*/
+  });
+
+
+
 });
--- client/src/app/racing-game/camera.service.ts
+++ client/src/app/racing-game/camera.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-// import { PerspectiveCamera, OrthographicCamera, Camera, Object3D } from 'three';
 import * as THREE from 'three';
 
 
@@ -11,11 +10,10 @@ const PERSPECTIVE_INITIAL_POSITION_Y: number = 10;
 const PERSPECTIVE_INITIAL_POSITION_Z: number = 30;
 
 // ORTHOGRAPHIC_CAMERA
-const ORTHOGRAPHIC_INITIAL_POSITION_Y: number = 100;
+export const ORTHOGRAPHIC_INITIAL_POSITION_Y: number = 100;
 const ORTHOGRAPHIC_FIELD_OF_VIEW: number = 50;
 const ORTHOGRAPHIC_CAMERA_NEAR_PLANE: number = 0;
 const ORTHOGRAPHIC_CAMERA_FAR_PLANE: number = 100;
-
 
 enum CameraType { PERSPECTIVE, ORTHOGRAPHIC }
 
@@ -23,14 +21,22 @@ enum CameraType { PERSPECTIVE, ORTHOGRAPHIC }
 export class CameraService {
 
     private camera: CameraType;
-    private orthographicCamera: THREE.OrthographicCamera;
-    private perspectiveCamera: THREE.PerspectiveCamera;
+    private _orthographicCamera: THREE.OrthographicCamera;
+    private _perspectiveCamera: THREE.PerspectiveCamera;
     private target: THREE.Object3D;
     private container: HTMLDivElement;
 
     public constructor() {
         this.camera = CameraType.PERSPECTIVE;
      }
+
+    public get perspectiveCamera(): THREE.PerspectiveCamera {
+        return this._perspectiveCamera;
+    }
+
+    public get orthographicCamera(): THREE.OrthographicCamera {
+        return this._orthographicCamera;
+    }
 
     public initialize(container: HTMLDivElement, target: THREE.Object3D): void {
         this.container = container;
@@ -39,20 +45,16 @@ export class CameraService {
     }
 
     public getCamera(): THREE.Camera {
-        return this.camera === CameraType.ORTHOGRAPHIC ? this.orthographicCamera : this.perspectiveCamera;
+        return this.camera === CameraType.ORTHOGRAPHIC ? this._orthographicCamera : this._perspectiveCamera;
     }
 
     public changeCamera(): void {
         this.camera === CameraType.PERSPECTIVE ? this.camera = CameraType.ORTHOGRAPHIC : this.camera = CameraType.PERSPECTIVE;
     }
 
-    private getAspectRatio(): number {
-      return this.container.clientWidth / this.container.clientHeight;
-    }
-
     public initializeCameras(): void {
         /*tslint:disable:no-magic-numbers */
-        this.orthographicCamera = new THREE.OrthographicCamera (
+        this._orthographicCamera = new THREE.OrthographicCamera (
             ORTHOGRAPHIC_FIELD_OF_VIEW / - 2,
             ORTHOGRAPHIC_FIELD_OF_VIEW / 2,
             ORTHOGRAPHIC_FIELD_OF_VIEW / this.getAspectRatio() / 2,
@@ -61,33 +63,40 @@ export class CameraService {
             ORTHOGRAPHIC_CAMERA_FAR_PLANE
         );
         /*tslint:enable:no-magic-numbers*/
-        this.orthographicCamera.position.x = this.target.position.x;
-        this.orthographicCamera.position.y = ORTHOGRAPHIC_INITIAL_POSITION_Y;
-        this.orthographicCamera.position.z = this.target.position.z;
+        this._orthographicCamera.position.x = this.target.position.x;
+        this._orthographicCamera.position.y = ORTHOGRAPHIC_INITIAL_POSITION_Y;
+        this._orthographicCamera.position.z = this.target.position.z;
 
-        this.orthographicCamera.lookAt(this.target.position);
+        this._orthographicCamera.lookAt(this.target.position);
 
 
-        this.perspectiveCamera = new THREE.PerspectiveCamera (
+        this._perspectiveCamera = new THREE.PerspectiveCamera (
             PERSPECTIVE_FIELD_OF_VIEW,
             this.getAspectRatio(),
             NEAR_CLIPPING_PLANE,
             FAR_CLIPPING_PLANE
         );
+        this._perspectiveCamera.position.x = this.target.position.x;
+        this._perspectiveCamera.position.y = this.target.position.y;
+        this._perspectiveCamera.position.z = this.target.position.z;
+
     }
 
     public updatePosition(): void {
-       this.orthographicCamera.position.x = this.target.position.x;
-       this.orthographicCamera.position.z = this.target.position.z;
+       this._orthographicCamera.position.x = this.target.position.x;
+       this._orthographicCamera.position.z = this.target.position.z;
 
 
        const relativeCameraOffset: THREE.Vector3 = new THREE.Vector3(0, PERSPECTIVE_INITIAL_POSITION_Y, PERSPECTIVE_INITIAL_POSITION_Z);
        const cameraOffset: THREE.Vector3 = relativeCameraOffset.applyMatrix4( this.target.matrix );
 
-       this.perspectiveCamera.position.x = cameraOffset.x;
-       this.perspectiveCamera.position.z = cameraOffset.z;
-       this.perspectiveCamera.position.y = cameraOffset.y;
-       this.perspectiveCamera.lookAt(this.target.position);
+       this._perspectiveCamera.position.x = cameraOffset.x;
+       this._perspectiveCamera.position.z = cameraOffset.z;
+       this._perspectiveCamera.position.y = cameraOffset.y;
+       this._perspectiveCamera.lookAt(this.target.position);
     }
 
+    private getAspectRatio(): number {
+        return this.container.clientWidth / this.container.clientHeight;
+    }
 }
