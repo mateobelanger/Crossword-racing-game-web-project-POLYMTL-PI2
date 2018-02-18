8e62752fdaae9e69ab438d87abab2b2385aea2b0
Jeremie Huppe <jeremie.huppe@polymtl.ca>
2018-02-18 12:52:34 -0500

    Finalized skybox.service : random skybox is generated when loading the game - RG - J.H.

--- client/src/app/racing-game/skybox.service.ts
+++ client/src/app/racing-game/skybox.service.ts
@@ -1,30 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Scene, CubeTextureLoader} from "three";
 
+const NUMBER_OF_SKYBOXES: number = 7;
+
 @Injectable()
 export class SkyboxService {
 
   private scene: Scene;
   private skyboxName: string;
+  private skyboxSate: string;
+  // TODO : string magique?
+  private skyboxes: string[] = ["clouds", "interstellar", "moon", "ocean",
+                                "sand", "storm", "sunset" ];
 
-  constructor() { }
+  public constructor() { }
 
   public initialization (scene: Scene): void {
     this.scene = scene;
-    this.skyboxName = "interstellar";
+    this.skyboxName = this.skyboxes[Math.floor(Math.random() * NUMBER_OF_SKYBOXES)];
+    this.skyboxSate = "day";
   }
-
-
 
   public generateSkybox(): void {
 
-    /*skyboxName: string
-    if (skyboxName) {
-      this.skyboxName = skyboxName;
-    }*/
-
     this.scene.background = new CubeTextureLoader()
-    .setPath ( '../../../assets/skybox/sand/' ) //.setPath ( '../../../assets/skybox/' + this.skyboxName + '/' )
+    .setPath ( '../../../assets/skybox/' + this.skyboxName + '/' + this.skyboxSate + '/')
     .load ( [
         'right.png',
         'left.png',
