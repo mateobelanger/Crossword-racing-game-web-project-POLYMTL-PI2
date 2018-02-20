e5632f75f1e5dbc0a310f4c973abcacdf3b78f01
JeHuppe <jeremie123go@gmail.com>
2018-02-20 10:36:19 -0500

    Changed string array to const - RG - J.H.

--- client/src/app/racing-game/skybox.service.ts
+++ client/src/app/racing-game/skybox.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import * as THREE from "three";
 import { LAND_WIDTH, LAND_HEIGHT, BACKGROUND_PLANE_POSITION_Y } from './constants';
 
-const NUMBER_OF_SKYBOXES: number = 7;
+const SKYBOXES: Array<string> = ["clouds", "interstellar", "moon", "ocean",
+                                 "sand", "storm", "sunset"];
+
 
 @Injectable()
 export class SkyboxService {
@@ -12,21 +14,17 @@ export class SkyboxService {
     private skyboxSate: string;
     private backgroundPlane: THREE.Mesh;
 
-    // TODO : string magique?
-    private skyboxes: string[] = ["clouds", "interstellar", "moon", "ocean",
-                                  "sand", "storm", "sunset"];
 
     public constructor() { }
 
     public initialization(scene: THREE.Scene): void {
       this.scene = scene;
-      this.skyboxName = this.skyboxes[Math.floor(Math.random() * NUMBER_OF_SKYBOXES)]; //
+      this.skyboxName = SKYBOXES[Math.floor(Math.random() * SKYBOXES.length)];
       this.skyboxSate = "day";
       this.generateBackgroundView();
     }
 
     public generateSkybox(): void {
-
       this.scene.background = new THREE.CubeTextureLoader()
         .setPath('../../../assets/skybox/' + this.skyboxName + '/' + this.skyboxSate + '/')
         .load([
@@ -37,12 +35,10 @@ export class SkyboxService {
           'back.png',
           'front.png'
         ]);
-
     }
 
     public generateBackgroundView(): void {
-      let texture: THREE.Texture;
-      texture = new THREE.TextureLoader().load('../../../assets/skybox/' + this.skyboxName + '/' + this.skyboxSate + '/bottom.png');
+      const texture: THREE.Texture = new THREE.TextureLoader().load('../../../assets/skybox/' + this.skyboxName + '/' + this.skyboxSate + '/bottom.png');
       const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
       this.backgroundPlane = new THREE.Mesh(new THREE.PlaneGeometry(LAND_WIDTH, LAND_HEIGHT), material);
       this.backgroundPlane.position.y = BACKGROUND_PLANE_POSITION_Y;
