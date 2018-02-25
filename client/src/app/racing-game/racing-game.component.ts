0d7033f276bdef19f7ee1e2db40a26d17687a1ae
Samuel Meilleur <samuel.meilleur@outlook.com>
2018-02-24 19:07:37 -0500

    Modified some of the CSS for the Track List View - RG - S.M.

--- client/src/app/racing-game/racing-game.component.ts
+++ client/src/app/racing-game/racing-game.component.ts
@@ -2,24 +2,23 @@ import { Component, AfterViewInit } from '@angular/core';
 import { TracksProxyService } from './tracks-proxy.service';
 import { TrackData } from '../../../../common/communication/trackData';
 @Component({
-  selector: 'app-racing-game',
-  templateUrl: './racing-game.component.html',
-  styleUrls: ['./racing-game.component.css']
+    selector: 'app-racing-game',
+    templateUrl: './racing-game.component.html',
+    styleUrls: ['./racing-game.component.css']
 })
+
 export class RacingGameComponent implements AfterViewInit {
-  private tracks: TrackData[];
+    public tracks: TrackData[];
 
-  public constructor( private proxy: TracksProxyService ) { }
+    public constructor(private proxy: TracksProxyService) { }
 
-  public async ngAfterViewInit(): Promise<void> {
+    public async ngAfterViewInit(): Promise<void> {
 
-    await this.proxy.initialize();
-    this.tracks = this.proxy.tracks;
-  }
+        await this.proxy.initialize();
+        this.tracks = this.proxy.tracks;
+    }
 
-  public selectModalId(button: HTMLElement, track: TrackData): void {
-    button.setAttribute("data-target", "#" + track.name);
-    console.log(button)
-  }
-
+    public selectModalId(button: HTMLElement, track: TrackData): void {
+        button.setAttribute("data-target", "#" + track.name);
+    }
 }
