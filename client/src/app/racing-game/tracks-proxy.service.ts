d32e2a8a8c3d55fb887fbc4fe47d68639b1921e9
Mathieu Bélanger <mathieu-2.belanger@polymtl.ca>
2018-02-25 22:17:25 -0500

    Correction valid/invalid popups + constraints on track name -RG - M.B

--- client/src/app/racing-game/tracks-proxy.service.ts
+++ client/src/app/racing-game/tracks-proxy.service.ts
@@ -7,53 +7,53 @@ const URI_MONGO_DB: string = "http://localhost:3000/service/mongoDB";
 @Injectable()
 export class TracksProxyService {
 
-  private _tracks: TrackData[] = [];
+    private _tracks: TrackData[] = [];
 
-  public constructor( private _http: HttpClient) { }
+    public constructor(private _http: HttpClient) { }
 
-  public async initialize(): Promise<void> {
-    await this.fetchTracks().then((data) => {this._tracks = data; });
-  }
-
-  public get tracks(): TrackData[] {
-    return this._tracks;
-  }
-
-  public async addTrack(track: TrackData): Promise<void> {
-    return this._http.post<TrackData>(URI_MONGO_DB, track).toPromise()
-    .then((validTrack: TrackData) => {this.tracks.push(validTrack); });
-  }
-
-  public async deleteTrack(trackName: string): Promise<void> {
-    const url: string = URI_MONGO_DB + "/" + trackName;
-
-    return this._http.delete<TrackData>(url).toPromise().then(() => {
-      this._tracks.splice(1, this._tracks.indexOf(this.findTrack(trackName)));
-    });
-  }
-
-  public async saveTrack(track: TrackData): Promise<void> {
-
-    if ( this.findTrack(track.name) !== null) {
-      return this._http.put<TrackData>(URI_MONGO_DB, track).toPromise().then(() => {});
-    } else {
-      return this.addTrack(track);
+    public async initialize(): Promise<void> {
+        await this.fetchTracks().then((data) => { this._tracks = data; });
     }
 
-  }
+    public get tracks(): TrackData[] {
+        return this._tracks;
+    }
 
-  public findTrack(trackName: string): TrackData {
-    let track: TrackData = null;
-    this._tracks.forEach((elem) => {
-      if (elem.name === trackName)
-        track = elem;
-    });
+    public async addTrack(track: TrackData): Promise<void> {
+        return this._http.post<TrackData>(URI_MONGO_DB, track).toPromise()
+            .then((validTrack: TrackData) => { this.tracks.push(validTrack); });
+    }
 
-    return track;
-  }
+    public async deleteTrack(trackName: string): Promise<void> {
+        const url: string = URI_MONGO_DB + "/" + trackName;
 
-  private async fetchTracks(): Promise<TrackData[]> {
-      return this._http.get<TrackData[]>(URI_MONGO_DB).toPromise();
-  }
+        return this._http.delete<TrackData>(url).toPromise().then(() => {
+            this._tracks.splice(1, this._tracks.indexOf(this.findTrack(trackName)));
+        });
+    }
+
+    public async saveTrack(track: TrackData): Promise<void> {
+
+        if (this.findTrack(track.name) !== null) {
+            return this._http.put<TrackData>(URI_MONGO_DB, track).toPromise().then(() => { });
+        } else {
+            return this.addTrack(track);
+        }
+
+    }
+
+    public findTrack(trackName: string): TrackData {
+        let track: TrackData = null;
+        this._tracks.forEach((elem) => {
+            if (elem.name === trackName)
+                track = elem;
+        });
+
+        return track;
+    }
+
+    private async fetchTracks(): Promise<TrackData[]> {
+        return this._http.get<TrackData[]>(URI_MONGO_DB).toPromise();
+    }
 
 }
