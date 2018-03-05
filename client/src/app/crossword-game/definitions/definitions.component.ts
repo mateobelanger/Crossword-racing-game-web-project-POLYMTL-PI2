a972dcd7bb3e990ac207f09db09baa812c4af689
Samuel Meilleur <samuel.meilleur@outlook.com>
2018-03-05 13:06:56 -0500

    Fixed a bug that didn't focus an input when clicking on a definition - CG - S.M.

--- client/src/app/crossword-game/definitions/definitions.component.ts
+++ client/src/app/crossword-game/definitions/definitions.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { WordService } from '../word.service';
 import { ValidatorService } from '../validator.service';
 import { Direction } from '../../../../../common/crosswordsInterfaces/word';
+import { GridService } from '../grid.service';
 
 
 @Component({
@@ -13,13 +14,14 @@ export class DefinitionsComponent {
     public horizontalDefinitions: string[][];
     public verticalDefinitions: string[][];
 
-    public constructor(private wordService: WordService, private validatorService: ValidatorService) {
+    public constructor(private wordService: WordService, private validatorService: ValidatorService, private gridService: GridService) {
         this.horizontalDefinitions = wordService.getDefinitions(Direction.HORIZONTAL);
         this.verticalDefinitions = wordService.getDefinitions(Direction.VERTICAL);
     }
 
     public onSelect(definition: string): void {
         this.wordService.definition = definition;
+        this.gridService.focusOnSelectedWord();
     }
 
     public isValidatedDefinition(definition: string): boolean {
