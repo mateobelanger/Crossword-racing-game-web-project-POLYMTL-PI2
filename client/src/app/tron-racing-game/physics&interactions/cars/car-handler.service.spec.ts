569500118b6fe5c0806e0840888c1f900977760a
Samuel Meilleur <mathieu-2.belanger@polymtl.ca>
2018-04-10 12:24:21 -0400

    Added test for shuffle of starting positions - RG - S.M.

--- client/src/app/tron-racing-game/physics&interactions/cars/car-handler.service.spec.ts
+++ client/src/app/tron-racing-game/physics&interactions/cars/car-handler.service.spec.ts
@@ -1,15 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
-
 import { CarHandlerService } from './car-handler.service';
 
 describe('CarHandlerService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CarHandlerService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [CarHandlerService]
+        });
     });
-  });
 
-  it('should be created', inject([CarHandlerService], (service: CarHandlerService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', inject([CarHandlerService], (service: CarHandlerService) => {
+        expect(service).toBeTruthy();
+    }));
 });
