50ecf5e6887672ce424fef18c4a8676d6817db7b
AlexandreRault <alexandre.rault@polymtl.ca>
2018-04-17 13:19:39 -0400

    Correction on a test following the refactoring of the controller - RG - A.R.

--- client/src/app/tron-racing-game/physics&interactions/cars/car-handler.service.spec.ts
+++ client/src/app/tron-racing-game/physics&interactions/cars/car-handler.service.spec.ts
@@ -3,11 +3,12 @@ import { CarHandlerService } from "./car-handler.service";
 import { SpeedZonesService } from "../../virtualPlayers/speed-zones.service";
 import { RaceProgressionHandlerService } from "../../raceData/raceProgression/race-progression-handler.service";
 import { TextureLoaderService } from "../../gameRendering/textureLoader/texture-loader.service";
+import { InputHandlerService } from "../controller/input-handler.service";
 
 describe("CarHandlerService", () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [CarHandlerService, SpeedZonesService, RaceProgressionHandlerService, TextureLoaderService]
+            providers: [CarHandlerService, SpeedZonesService, RaceProgressionHandlerService, TextureLoaderService, InputHandlerService]
         });
     });
 
