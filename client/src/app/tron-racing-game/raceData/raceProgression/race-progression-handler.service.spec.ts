eb967a92370f9c5ebc982878775a298971aebf33
poclecoqq <pierre-olivier.cote@polymtl.ca>
2018-04-18 20:44:16 -0400

    fixed tslint issues - RG - POC

--- client/src/app/tron-racing-game/raceData/raceProgression/race-progression-handler.service.spec.ts
+++ client/src/app/tron-racing-game/raceData/raceProgression/race-progression-handler.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed, inject } from "@angular/core/testing";
 import * as THREE from "three";
-import {USERNAME} from "../../constants";
 import { RaceProgressionHandlerService } from "./race-progression-handler.service";
 
 describe("RaceProgressionHandlerService", () => {
@@ -20,12 +19,13 @@ describe("RaceProgressionHandlerService", () => {
             "bob0": new THREE.Vector3(10, 10, 10),
             "bob1": new THREE.Vector3(12, 14, 109),
             "bob2": new THREE.Vector3(14, 2, 9),
-            USERNAME: new THREE.Vector3(0, 0, 0),
+            "user": new THREE.Vector3(0, 0, 0),
 
         };
-        const waypoints: [number, number, number][] = [[0, 0, 0], [100, 100, 100]]
+        const waypoints: [number, number, number][] = [[0, 0, 0], [100, 100, 100]];
         // tslint:enable:no-magic-numbers
-        service.initialize(carPositions, waypoints);
+        service.initialize(carPositions, waypoints).then(   () => {},
+                                                            () => {});
         expect(service.isUserFirst()).toBeFalsy();
     }));
 });
